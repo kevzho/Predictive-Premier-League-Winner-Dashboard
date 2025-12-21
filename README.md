3475e54beb27ea08c701591eb3f3941abed8040a
Kevin Zhou <kevinz09302009@gmail.com>
2025-12-21 17:06:07 -0500

    updated fetch_data.py, started with some visualization

--- README.md
+++ README.md
@@ -5,3 +5,22 @@
 - Visualizes model outputs (e.g., probabilities, rankings, expected points)
 - Uses machine learning or Monte-Carlo simulation
 - Runs locally or deploys via Streamlit Cloud
+
+### Sources: 
+1. https://www.football-data.co.uk/englandm.php
+2.  
+
+
+### Data that was collected:
+#### From each match:
+- Home team, Away team
+- Score
+- xG (Expected Goals)
+- Possession, shots, cards
+- Match date
+- (Optional, will maybe include) Betting odds
+
+#### From each team: 
+- Current standings
+- Form (Last 5 wins)
+- SPI Standings (If we can access)
---
+++ fetch_data.py
@@ -0,0 +1,47 @@
+#fetches data remotely
+
+import os
+import pandas as pd
+import requests
+
+from datetime import datetime
+
+#our data directory
+DATA_DIR = "data"
+
+''' For future use, possibly, if we were to scrape multiple seasons
+def season_codes_candidates():
+    # returns candidate season like '2526' for 2025-2026 
+    now = datetime.utcnow() 
+    year = now.year
+    month = now.month
+    
+    if month >= 7: # season starts in Jul
+        start = year % 100
+        end = (year + 1) % 100
+    else:
+        start = (year - 1) % 100
+        end = year % 100
+
+'''
+#gets the csv from football-data.co.uk for given season and league
+def download_fdc_csv(season_code = "2526", league = "E0"):
+    url = f"https://www.football-data.co.uk/mmz4281/{season_code}/{league}.csv"
+    out = f"{DATA_DIR}/{league}_{season_code}.csv"
+    
+    print("Downloading: ", url)
+    # set timeout to 10 seconds
+    r = requests.get(url, timeout = 10)
+
+    #If status code is not 200, print error (meaning that it was unsuccessful)
+    if r.status_code != 200:
+        print("Error: ", r.status_code)
+        return 
+    
+    #exporting to output file
+    with open(out, "wb") as f:
+        f.write(r.content)
+    
+    print("Saved to: ", out)
+
+download_fdc_csv("2526", "E0") #downloading 25-26 premier league data
